05bcffee8f82b9524ebd511400ba3f48b9aacb61
Marco Firmino <marcoaf72@bol.com.br>
2022-02-18 18:26:31 -0300

    Produto e associação muitos para muitos

---
+++ src/main/java/com/marco/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,13 @@
+package com.marco.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.marco.cursomc.domain.Produto;
+
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+	
+}
